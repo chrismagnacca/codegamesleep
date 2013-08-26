3b2ad92a075447e03273449bfc85b96953ed8eba
Chris Magnacca <chrismagnacca@gmail.com>
2013-08-25 20:36:56 -0400

    favicon patches

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module Blog
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true;
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
